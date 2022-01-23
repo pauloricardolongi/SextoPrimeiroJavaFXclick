c7c36ae3d536ac33f66d0b09f4fd1cc127ee3b4f
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-23 02:47:24 -0300

    label

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,18 +1,37 @@
 package gui;
 
-import gui.util.Alerts;
+import java.util.Locale;
+
+
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
 	
 	@FXML
-	private Button btTest;
+	private TextField txtNumber1;
 	
 	@FXML
-	public void onBtTesAction() {
-		Alerts.showAlert("Alert title", null, "Hello", AlertType.ERROR);
+	private TextField txtNumber2;
+	
+	@FXML
+	private Label labelResult;
+	
+	@FXML
+	private Button btSum;
+	
+	@FXML
+	public void onBtSumAction() {
+		
+		Locale.setDefault(Locale.US);
+		double number1 =  Double.parseDouble(txtNumber1.getText());
+		double number2 = Double.parseDouble(txtNumber2.getText());
+		double sum = number1 + number2;
+		labelResult.setText(String.format("%.2f", sum));
+		
 	}
 
 }
